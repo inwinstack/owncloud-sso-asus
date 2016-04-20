2fe5d076f89bcc013801968777f36aac708404ac
DaubaKao <dauba.k@inwinstack.com>
2016-04-20 18:16:48 +0800

    [update] add authinfo.php implement iauthoinfo

---
+++ authinfo.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OCA\SingleSignOn;
+
+/**
+ * Class AuthInfo
+ * @author Dauba
+ */
+class AuthInfo implements IAuthInfo
+{
+    /**
+     * requeir keys for auth info
+     *
+     * @var array
+     */
+    //private static $requireKeys = array("key","userid");
+    private static $requireKeys = array("token1");
+
+    /**
+     * auth info
+     *
+     * @var array
+     */
+    private static $info = array();
+
+    /**
+     * set auth info
+     *
+     * @return void
+     */
+    public static function init()
+    {
+        $request = \OC::$server->getRequest();
+        $session = \OC::$server->getSession();
+        foreach (self::$requireKeys as $key) {
+            if($request->offsetGet($key)) {
+                self::$info[$key] = $request->offsetGet($key);
+            }
+            else if($session->get("sso_" . $key)) {
+                self::$info[$key] = $session->get("sso_" . $key);
+            }
+        }
+        self::$info["userIp"] = $request->getRemoteAddress();
+    }
+
+    /**
+     * Getter for Info
+     *
+     * @return array
+     */
+    public static function get()
+    {
+        foreach (self::$requireKeys as $key) {
+            if(!array_key_exists($key, self::$info)) {
+                return null;
+            }
+        }
+        return self::$info;
+    }
+    
+}
