e6abd2478351ca4a200854ffdf77acccb479b8eb
DaubaKao <dauba.k@inwinstack.com>
2016-12-12 17:38:53 +0800

    [update] webdavlogin

--- webdavauthinfo.php
+++ webdavauthinfo.php
@@ -10,32 +10,29 @@ class WebDavAuthInfo implements IWebDavAuthInfo
 {
     
     /**
+    * requeir keys for auth info
+    *
+    * @var array
+    **/
+    private static $requireKeys = array("token1", "userIp");
+
+    /**
      * auth info
      *
      * @var array
      */
     private static $info = array();
-
-    /**
-     * undocumented function
-     *
-     * @return void
-     */
-    public static function init($userID, $password)
-    {
-        $request = \OC::$server->getRequest();
-
-        self::$info["userIp"] = $request->getRemoteAddress();
-        self::$info["token1"] = RequestManager::send(ISingleSignOnRequest::GETTOKEN, array("userId" => $userID, "password" => $password, "userIp" => self::$info["userIp"]));
-    }
     
     /**
      * Getter for Info
      *
      * @return array
      */
-    public static function get()
+    public static function get($userID, $password)
     {
+        self::$info["userIp"] = $request->getRemoteAddress();
+        self::$info["token1"] = RequestManager::send(ISingleSignOnRequest::GETTOKEN, array("userId" => $userID, "password" => $password, "userIp" => self::$info["userIp"]));
+
         foreach (AuthInfo::$requireKeys as $key) {
             if(!array_key_exists($key, self::$info)) {
                 return null;
